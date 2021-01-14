15ba0769202b3d47d0cecaa8e12596cf99d7ae56
Phenyo Pilatso <pilatsophenyo@gmail.com>
2021-01-14 16:05:10 +0200

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { DropDownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
     ShoppingListComponent,
     ShoppingEditComponent,
     RecipeListComponent,
-    RecipeItemComponent
+    RecipeItemComponent, 
+    DropDownDirective
   ],
   imports: [
     BrowserModule
