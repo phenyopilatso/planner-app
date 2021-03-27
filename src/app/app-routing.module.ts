77057fc19e181d6a49046d34cc8078e78ee35c87
phpilatso <pilatsophenyo@gmail.com>
2021-03-27 21:48:15 +0200

    commit -1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'; 
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
+
 import { AuthGuard } from './auth/auth.guard';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
@@ -27,7 +27,6 @@ const appRoutes: Routes = [
         ]
     },
     {path: 'shopping-list', component: ShoppingListComponent},
-    {path: 'auth', component: AuthComponent}
 ]
 
 @NgModule({
